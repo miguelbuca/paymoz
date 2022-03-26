90f18e0962fd1fbf846fd67cd9f6ff27eb4bd7bb
Miguel Buca <miguelpedrobuca@gmail.com>
2022-03-26 22:07:39 +0100

    readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# paymoz
+# PayMoz - Test
+
+Preview [PayMoz.](https://paymoz.netlify.app/)
+
+> Start project
+
+###### Run
+
+- yarn
+- yarn dev
--- src/components/pages/session/styles.ts
+++ src/components/pages/session/styles.ts
@@ -5,6 +5,7 @@ export const SessionContainer = styled.div`
   width: 512px;
   height: 100%;
   background-color: ${({ theme }: any) => theme.light.bgColor};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 10px;
 
   @media (max-width: 768px) {
